d712c4efdcbd6a64732c2065a6b6d8bbfb1db42e
symoleon <symoleon97@gmail.com>
2021-08-12 22:20:55 +0200

    Add warn command

---
+++ commands/Moderation/warn.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'warn',
+	category: 'Moderation',
+	description: 'Warn an user',
+	usage: '<user> <description>',
+	permission: '',
+	async execute(message, commandArguments) {
+		const messageEmbed = new Discord.MessageEmbed();
+		if (commandArguments.length >= 2) {
+			const memberResolvable = commandArguments.shift().replace(/<|>|@|!/g, '');
+			const member = message.guild.members.resolve(memberResolvable);
+			if (member != null) {
+				const warningDescription = commandArguments.join(' ');
+				const arcanusGuild = await message.client.arcanusClient.getGuild(message.guild.id);
+				const arcanusGuildMember = await message.client.arcanusClient.getGuildMember(arcanusGuild, member.id);
+				await arcanusGuildMember.warningsManager.warn(message.author.id, warningDescription);
+				messageEmbed.setTitle('Warned!');
+				messageEmbed.setDescription(`Warned user \`${member.user.username}\` for \`${warningDescription}\``);
+			} else {
+				messageEmbed.setTitle('Invalid user!');
+				messageEmbed.setDescription(`I can't find user with \`${memberResolvable}\` name or ID.`);
+			}
+		} else {
+			messageEmbed.setTitle('Not enough arguments!');
+			messageEmbed.setDescription('Provide additional arguments or use `help warn` command.');
+		}
+		message.channel.send(messageEmbed);
+	},
+};
