2d610695e399c21cf3bb2d01b674a294855d45ac
symoleon <symoleon97@gmail.com>
2021-08-13 10:57:51 +0200

    Fix error handling

--- commands/Moderation/warn.js
+++ commands/Moderation/warn.js
@@ -9,21 +9,25 @@ module.exports = {
 		const messageEmbed = new Discord.MessageEmbed();
 		if (commandArguments.length >= 2) {
 			const memberResolvable = commandArguments.shift().replace(/<|>|@|!/g, '');
-			const member = await message.guild.members.fetch(memberResolvable);
-			if (member != null) {
+			try {
+				const member = await message.guild.members.fetch(memberResolvable);
 				const warningDescription = commandArguments.join(' ');
 				const arcanusGuild = await message.client.arcanusClient.getGuild(message.guild.id);
 				const arcanusGuildMember = await message.client.arcanusClient.getGuildMember(arcanusGuild, member.id);
 				await arcanusGuildMember.warningsManager.warn(message.author.id, warningDescription);
 				messageEmbed.setTitle('Warned!');
 				messageEmbed.setDescription(`Warned user \`${member.user.username}\` for \`${warningDescription}\``);
-			} else {
-				messageEmbed.setTitle('Invalid user!');
-				messageEmbed.setDescription(`I can't find user with \`${memberResolvable}\` name or ID.`);
+			} catch (error) {
+				if (error.code == 10013) {
+					messageEmbed.setTitle('Invalid user!');
+					messageEmbed.setDescription(`I can't find user with \`${memberResolvable}\` name or ID.`);
+				} else {
+					throw error;
+				}
 			}
 		} else {
 			messageEmbed.setTitle('Not enough arguments!');
-			messageEmbed.setDescription('Provide additional arguments or use `help warn` command.');
+			messageEmbed.setDescription(`Provide additional arguments or use \`help ${this.name}\` command.`);
 		}
 		message.channel.send(messageEmbed);
 	},
