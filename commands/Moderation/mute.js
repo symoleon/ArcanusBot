b4ce6be50c9647bb54fbc0eb1e10c1e1818e5c67
symoleon <symoleon97@gmail.com>
2021-08-29 01:06:41 +0200

    Add log channel

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -41,6 +41,15 @@ module.exports = {
 					const arcanusGuildMember = await message.client.arcanusClient.getGuildMember(arcanusGuild, member.id);
 					if (!arcanusGuildMember.muteId) {
 						await arcanusGuild.mutesManager.mute(arcanusGuildMember, message.author.id, muteDescription, durationInSeconds);
+						if (message.guild.channels.cache.has(arcanusGuild.mod_log_channel.toString())) {
+							const logEmbed = new Discord.MessageEmbed();
+							logEmbed.setAuthor(`${message.author.username}#${message.author.discriminator} (${message.author.id})`, message.author.avatarURL());
+							logEmbed.setDescription(`**Muted:** ${member.user.username}#${member.user.discriminator} (${member.user.id})!\n**Reason:** ${muteDescription}`);
+							logEmbed.setThumbnail(member.user.avatarURL());
+							logEmbed.setFooter(`Duration: ${durationReadable}`);
+
+							message.guild.channels.cache.get(arcanusGuild.mod_log_channel.toString()).send(logEmbed);
+						}
 						messageEmbed.setTitle('Muted!');
 						messageEmbed.setDescription(`Muted user \`${member.user.username}\` for \`${muteDescription}\`\nDuration: \`${durationReadable}\``);
 					} else {
