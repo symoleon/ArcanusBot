cd1b941f4860c560159b5fa12d9605ca6317af1f
symoleon <symoleon97@gmail.com>
2021-08-29 16:14:34 +0200

    Add role assignment and removal

--- events/tenSeconds.js
+++ events/tenSeconds.js
@@ -11,12 +11,13 @@ module.exports = {
 				await arcanusGuild.mutesManager.delete(mute.id);
 				try {
 					const guild = await client.guilds.fetch(arcanusGuild.id);
+					const member = await guild.members.fetch(mute.user_id.toString());
+					await member.roles.remove(arcanusGuild.mute_role_id.toString());
 					if (guild.channels.cache.has(arcanusGuild.mod_log_channel.toString())) {
-						const user = await client.users.fetch(mute.user_id.toString());
 						const logEmbed = new Discord.MessageEmbed();
 						logEmbed.setAuthor(`${client.user.username}#${client.user.discriminator} (${client.user.id})`, client.user.avatarURL());
-						logEmbed.setDescription(`**Unmuted:** ${user.username}#${user.discriminator} (${user.id})\n**Reason:** Mute expired!`);
-						logEmbed.setThumbnail(user.avatarURL());
+						logEmbed.setDescription(`**Unmuted:** ${member.user.username}#${member.user.discriminator} (${member.user.id})\n**Reason:** Mute expired!`);
+						logEmbed.setThumbnail(member.user.avatarURL());
 
 						guild.channels.cache.get(arcanusGuild.mod_log_channel.toString()).send(logEmbed);
 					}
