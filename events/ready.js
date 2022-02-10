c1f197eb175cd44d9ac8227ca2fe61c812885efc
symoleon <symoleon97@gmail.com>
2022-02-10 18:02:56 +0100

    Change log order

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,10 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(loggedClient, config, client) {
+		console.log(`Logged as: ${client.user.tag}`);
+		
 		const connected = await client.arcanusClient.Connect();
 		if (connected) console.log('Connected to database');
-		console.log(`Logged as: ${client.user.tag}`);
 
 		config.botMention = `<@${client.user.id}>`;
 		config.botMentionWithExclamationMark = `<@!${client.user.id}>`;
