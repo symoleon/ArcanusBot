e7dc7e815fcba865e2f9128d10bccb80e7dec230
symoleon <symoleon97@gmail.com>
2022-02-10 19:35:24 +0100

    Refactor code to use new API

--- commands/Moderation/warn.js
+++ commands/Moderation/warn.js
@@ -17,16 +17,15 @@ module.exports = {
 			try {
 				const member = await message.guild.members.fetch(memberResolvable);
 				const warningDescription = commandArguments.join(' ');
-				const arcanusGuild = await message.client.arcanusClient.getGuild(message.guild.id);
-				const arcanusGuildMember = await message.client.arcanusClient.getGuildMember(arcanusGuild, member.id);
-				await arcanusGuildMember.warningsManager.warn(message.author.id, warningDescription);
-				if (message.guild.channels.cache.has(arcanusGuild.mod_log_channel.toString())) {
+				const arcanusGuild = await message.client.arcanusClient.guilds.fetch(message.guild.id);
+				await arcanusGuild.warnings.create(member.id, message.author.id, warningDescription);
+				if (message.guild.channels.cache.has(arcanusGuild.modLogChannel.toString())) {
 					const logEmbed = new Discord.MessageEmbed();
 					logEmbed.setAuthor(`${message.author.username}#${message.author.discriminator} (${message.author.id})`, message.author.avatarURL());
 					logEmbed.setDescription(`**Warned:** ${member.user.username}#${member.user.discriminator} (${member.user.id})!\n**Reason:** ${warningDescription}`);
 					logEmbed.setThumbnail(member.user.avatarURL());
 
-					message.guild.channels.cache.get(arcanusGuild.mod_log_channel.toString()).send({ embeds: [logEmbed] });
+					message.guild.channels.cache.get(arcanusGuild.modLogChannel.toString()).send({ embeds: [logEmbed] });
 				}
 				response.setType('SUCCESS')
 				response.setTitle('Warned!');
